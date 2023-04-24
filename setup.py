865d721bb74a0cab6614f3826239fc1d80569fba
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2023-04-25 07:27:26 +0900

    skeleton

---
+++ setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+try:
+    with open('README.md', 'r') as f:
+        readme = f.read()
+except Exception:
+    readme = str('')
+
+
+install_requires = [
+    'einops',
+    'numpy >= 1.2',
+    'matplotlib',
+    'opencv-python',
+    'tqdm',
+    'pytest',
+]
+
+
+setup(
+    name='igniter',
+    version='0.0.1',
+    long_description=readme,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=install_requires,
+    test_suite='tests',
+)
